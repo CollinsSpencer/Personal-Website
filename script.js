318c0560f68a1e1c5d33efafba7409425e72d62e
CollinsSpencer <CollinsSpencer97@gmail.com>
2017-01-20 01:19:58 -0600

    V1.0.0
    Initial Website

---
+++ script.js
@@ -0,0 +1,14 @@
+<!--
+
+function onLoad(){
+	document.body.style.backgroundColor="lightgrey";
+	
+}
+
+window.onload = onLoad;
+var side = document.getElementById('sidebar');
+var back = document.createElement('p');
+	back.innerHTML = 'test12345678';
+
+document.body.insertBefore(back, document.body.firstChild);
+// -->
