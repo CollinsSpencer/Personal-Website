073efa0af493290e180c5253de814e9ec677b1c5
CollinsSpencer <CollinsSpencer97@gmail.com>
2017-01-20 01:53:15 -0600

    V2.4.0
    Updates. Added icon.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="favicon.ico" rel="shortcut icon" type="image/x-icon"/>
 <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="style.css">
 <link rel="stylesheet" type="text/css" href="genericons/genericons.css">
