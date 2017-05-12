e91af0f9b7a0195bd1b195f1597d36c2c5fc8fa7
Spencer Collins <collinsspencer97@gmail.com>
2017-05-12 16:59:45 -0500

    sample flex-layout on contact page

--- contact/index.php
+++ contact/index.php
@@ -11,6 +11,68 @@
   <!-- NAV -->
   <?php $selected = "contact"; require_once "../nav.php" ?>
 
+  <!-- CONTACT -->
+  <div class="centered">
+    <section class="horizontal horizontal-2 flex">
+      <article>
+        <section class="vertical vertical-2 flex">
+          <article style="min-height:50px;background-color:red"></article>
+          <article style="min-height:50px;background-color:blue"></article>
+        </section>
+      </article>
+
+      <article style="min-height:50px;background-color:grey">
+        <!-- Contact form -->
+        <section id="contact">
+          <div class="darkish container-fluid">
+            <div class="row bottom text-center">
+              <h1 class="heading">Contact Me</h1>
+              <h4>If you have any questions or would like to get in contact with me, feel free to use the following form.</h4>
+            </div>
+            <form id="contactForm" method="post" action="">
+              <section class="vertical flex">
+                <article>
+                  <section class="horizontal horizontal-2 flex">
+                    <article>
+                      <input id="firstName" class="form-control" type="text" name="fields[first_name]" placeholder="First Name">
+                    </article>
+                    <article>
+                      <input id="lastName" class="form-control" type="text" name="fields[last_name]" placeholder="Last Name">
+                    </article>
+                  </section>
+                </article>
+                <article>
+                  <input id="email" class="form-control" type="email" name="fields[email]" placeholder="Email">
+                </article>
+                <article>
+                  <input id="subject" class="form-control" type="text" name="fields[subject]" placeholder="Subject">
+                </article>
+                <article>
+                  <textarea id="message" class="form-control" name="fields[message]" placeholder="Message" rows="3" cols="50"></textarea>
+                </article>
+                <article>
+                  <section class="horizontal horizontal-2 flex">
+                    <article>
+                      <input id="submit" class="form-control" type="submit" name="fields[send]" value="Send">
+                    </article>
+                    <article>
+                      <input id="reset" class="form-control" type="reset" name="fields[reset]" value="Reset">
+                    </article>
+                  </section>
+                </article>
+              </section>
+            </form>
+            <div class="row bottom text-center">
+              <h5>You will also receive a copy of your message.</h5>
+            </div>
+          </div>
+        </section>
+      </article>
+    </section>
+  </div>
+
+
+
   <div style="height: 2000px">
     <h1>Hello World!<h1>
   </div>
