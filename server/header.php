dcc06c74de9f60c547607d1796f6e0d7ecf4eba8
Spencer Collins <collinsspencer97@gmail.com>
2017-05-12 16:21:21 -0500

    switch to flex-layout

--- server/header.php
+++ server/header.php
@@ -6,7 +6,10 @@
 <link href="/assets/resources/favicon.ico" rel="shortcut icon" type="image/x-icon"/>
 <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
     <!-- Latest compiled and minified CSS -->
+<!--
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+-->
 <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
 <link rel="stylesheet" type="text/css" href="/assets/resources/genericons/genericons.css">
 <link rel="stylesheet" href="/assets/css/nav.css">
+<link rel="stylesheet" href="/assets/css/flex-layout.css">
